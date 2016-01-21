88faebddec1fea953c72a6578b1978cef64406cd
Alex Eng <aeng@redhat.com>
2016-01-21 14:43:23 +1000

    Add comments on annotation

--- common/src/main/java/org/zanata/helper/common/annotation/TranslationServerPlugin.java
+++ common/src/main/java/org/zanata/helper/common/annotation/TranslationServerPlugin.java
@@ -6,6 +6,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * To be use with {@link org.zanata.helper.common.plugin.TranslationServerExecutor}
+ *
  * @author Alex Eng <a href="mailto:aeng@redhat.com">aeng@redhat.com</a>
  */
 @Retention(RetentionPolicy.RUNTIME)
